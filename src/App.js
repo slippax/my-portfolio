2749b10636ec72ccc925855f461e788ff3c3fee6
Stephen Lippa <you@example.com>
2020-07-04 13:07:13 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit me <code>src/App.js</code> and save to reload.
+          Edit me pls <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
