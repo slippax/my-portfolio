1f47a6a13eeff13920d5bf15d6f2b67e8875820e
Stephen Lippa <you@example.com>
2020-07-14 16:41:48 -0400

    Updates

---
+++ precache-manifest.87511a79f09be4fb8b19227e699f66b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96eb9ae84738cf04ed919f0c4a952570",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "1243fb6420449ead441d",
+    "url": "/My-Portfolio/static/css/2.f60733f3.chunk.css"
+  },
+  {
+    "revision": "a82e7059a96fc3c59f6e",
+    "url": "/My-Portfolio/static/css/main.59c97280.chunk.css"
+  },
+  {
+    "revision": "1243fb6420449ead441d",
+    "url": "/My-Portfolio/static/js/2.ef8ab78a.chunk.js"
+  },
+  {
+    "revision": "0b14dac90b918c082faed0d9d1548202",
+    "url": "/My-Portfolio/static/js/2.ef8ab78a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a82e7059a96fc3c59f6e",
+    "url": "/My-Portfolio/static/js/main.4465db5e.chunk.js"
+  },
+  {
+    "revision": "cc8fb5c09ef196e1df09",
+    "url": "/My-Portfolio/static/js/runtime-main.2dca773a.js"
+  },
+  {
+    "revision": "67fedf29fa5fd97687601ccd6abc0ae2",
+    "url": "/My-Portfolio/static/media/ezsens.67fedf29.JPG"
+  }
+]);
