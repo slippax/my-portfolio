168f2c2e2f91a678b438a2f099ce47023a13ed9c
Stephen Lippa <you@example.com>
2020-07-15 15:49:11 -0400

    Updates

---
+++ precache-manifest.8f61a67bf40f556751a8e7147284f8e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a1485e4f1a4553793172d0ba570405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca3822a9ca7070b459b",
+    "url": "/static/css/2.ab54e29e.chunk.css"
+  },
+  {
+    "revision": "0e1ea0682bc8195a7d24",
+    "url": "/static/css/main.d564d07a.chunk.css"
+  },
+  {
+    "revision": "6ca3822a9ca7070b459b",
+    "url": "/static/js/2.9a708908.chunk.js"
+  },
+  {
+    "revision": "382f5d92564f5cb9b5ce43737b4af4c9",
+    "url": "/static/js/2.9a708908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e1ea0682bc8195a7d24",
+    "url": "/static/js/main.0ad5c6a5.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "67fedf29fa5fd97687601ccd6abc0ae2",
+    "url": "/static/media/ezsens.67fedf29.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6106c6eef6ea7be1606baaee881b465a.js"
+  "/precache-manifest.8f61a67bf40f556751a8e7147284f8e0.js"
 );
 
 self.addEventListener('message', (event) => {
