ad5d1a7674f9c1e0e7fe6d2c1540f64a41976c95
Stephen Lippa <you@example.com>
2020-07-15 13:58:29 -0400

    Updates

---
+++ precache-manifest.f6aac9f8b2bd99945f420c7e3025361e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875e6cfab60baaa5b4fe5d6537f90ce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d7146cd13cd77fa8456",
+    "url": "/static/css/2.f60733f3.chunk.css"
+  },
+  {
+    "revision": "6990272f2c05fa006ae9",
+    "url": "/static/css/main.423ab6b0.chunk.css"
+  },
+  {
+    "revision": "8d7146cd13cd77fa8456",
+    "url": "/static/js/2.e8e2e093.chunk.js"
+  },
+  {
+    "revision": "382f5d92564f5cb9b5ce43737b4af4c9",
+    "url": "/static/js/2.e8e2e093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6990272f2c05fa006ae9",
+    "url": "/static/js/main.1f3e687d.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "67fedf29fa5fd97687601ccd6abc0ae2",
+    "url": "/static/media/ezsens.67fedf29.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.7f3ee08925608fcd6f19c3b4679e2c71.js"
+  "/precache-manifest.f6aac9f8b2bd99945f420c7e3025361e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
