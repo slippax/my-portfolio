a4b5875ba1ad5f238b4be77fd2f9cf1971284edd
Stephen Lippa <you@example.com>
2020-07-27 15:40:54 -0400

    Updates

--- precache-manifest.5f9b6fe1c9a8534c5fc5ba3e9059e15c.js
+++ precache-manifest.5f9b6fe1c9a8534c5fc5ba3e9059e15c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d6fe75e1310b524cea6798b6e50d2f76",
+    "revision": "9ab918124ec9ecc69eefb0cc2d52d3a6",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.ab54e29e.chunk.css"
   },
   {
-    "revision": "c28bdb05f33dbb972d83",
+    "revision": "2a93136f08392788e5be",
     "url": "/static/css/main.c04e7032.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2b0495f1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c28bdb05f33dbb972d83",
-    "url": "/static/js/main.a1cd7d47.chunk.js"
+    "revision": "2a93136f08392788e5be",
+    "url": "/static/js/main.039fe1c4.chunk.js"
   },
   {
     "revision": "efe25b4b8d1f7af9da4b",
