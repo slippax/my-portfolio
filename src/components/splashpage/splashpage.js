ebdacf78b24f3e91d1b66b2bc37bb01d156a42ed
Stephen Lippa <you@example.com>
2020-10-30 12:16:20 -0400

    animation timing splashpage

--- src/components/splashpage/splashpage.js
+++ src/components/splashpage/splashpage.js
@@ -4,10 +4,11 @@ import classes from "./splashpage.module.css";
 import Typist from "react-typist";
 import { NextButton } from '../portfolio/ui/button/button';
 import ReactScrollWheelHandler from "react-scroll-wheel-handler";
-import Roll from "react-reveal/Roll";
-import Flip from "react-reveal/Zoom";
+import Rotate from 'react-reveal/Zoom';
+import Slide from 'react-reveal/Rotate';
 import Fade from "react-reveal/Fade";
 import Tada from 'react-reveal/Jello';
+import Bounce from 'react-reveal/Bounce';
 import Pulse from 'react-reveal/Pulse';
 import { SocialMediaIconsReact } from "social-media-icons-react";
 function SplashPage() {
@@ -16,40 +17,59 @@ function SplashPage() {
   const [git, wiggleGit] = useState(false);
   const [next, wiggleNext] = useState(false);
   const [box, wiggleBox] = useState(false);
+  const [greet1, setgreet1] = useState(false);
+  const [greet2, setgreet2] = useState(false);
+  const [greet3, setgreet3] = useState(false);
+  const [greet4, setgreet4] = useState(false);
+  setTimeout(() => {
+    setgreet1(true);
+  }, 1100);
+  setTimeout(() => {
+    setgreet2(true);
+  }, 2100);
+  setTimeout(() => {
+    setgreet3(true);
+  }, 3500);
+  setTimeout(() => {
+    setgreet4(true);
+  }, 4000);
   return (
     <div onMouseEnter={() => wiggleBox(!box)}>
+      
       <div className={classes.container}>
         <ReactScrollWheelHandler className={classes.handlerbox}
           downHandler={() => navigate("/portfolio", { replace: true })}>
+            <Rotate>
           <div className={classes.headerText}>
             <Pulse spy={box}>
-              <Roll bottom cascade>
+              {greet1 ? (<Fade right cascade>
                 <p className={classes.greetingText}>Hi, my name is</p>
-                <p className={classes.nameText}>Stephen Lippa</p>
-              </Roll>
-              <Typist avgTypingDelay={60}>
-                <Typist.Delay ms={2600} />
+              </Fade>) : (<p className={classes.greetingText2}>.</p>)}
+              {greet2 ? (<Slide cascade><p className={classes.nameText}>Stephen Lippa</p></Slide>) : (<p className={classes.nameText2}>.</p>)}
+
+              <Typist avgTypingDelay={65}>
+                <Typist.Delay ms={5000} />
                 <span className={classes.taglineText2}>
                   I am a front end developer.
               </span>
                 <Typist.Delay ms={1000} />
-                <Typist.Backspace count={27} />
+                <Typist.Backspace count={25} />
                 <span className={classes.taglineText}>
-                  I build things for the web.
+                  build things for the web.
               </span>
               </Typist>
             </Pulse>
-            <Fade bottom>
-              <div className={classes.button} onMouseEnter={() => wiggleNext(!next)}>
+            {greet3 ? (<div className={classes.button} onMouseEnter={() => wiggleNext(!next)}>
                 <Tada spy={next}>
+                  <Bounce>
                   <NextButton />
+                  </Bounce>
                 </Tada>
-              </div>
-            </Fade>
-            <div className={classes.icontainer}>
+              </div>) : (<div/>)}
+              {greet4 ? (<div className={classes.icontainer}>
               <Tada spy={git}>
                 <div className={classes.icongit} onMouseEnter={() => wiggleGit(!git)}>
-                  <Flip>
+                  <Bounce>
                     <SocialMediaIconsReact
                       borderColor="rgba(0,0,0,0.25)"
                       borderWidth="3"
@@ -62,12 +82,12 @@ function SplashPage() {
                       roundness="50%"
                       size="60"
                     />
-                  </Flip>
+                  </Bounce>
                 </div>
               </Tada>
               <div className={classes.iconlinked} onMouseEnter={() => wiggleLinked(!linked)}>
                 <Tada spy={linked}>
-                  <Flip>
+                  <Bounce>
                     <SocialMediaIconsReact
                       borderColor="rgba(0,0,0,0.25)"
                       borderWidth="3"
@@ -80,11 +100,13 @@ function SplashPage() {
                       roundness="50%"
                       size="60"
                     />
-                  </Flip>
+                  </Bounce>
                 </Tada>
               </div>
+            </div>) : (<div/>)}
+              
             </div>
-          </div>
+            </Rotate>
         </ReactScrollWheelHandler>
       </div>
     </div>
